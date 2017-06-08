2ea705dca9958ed246bb74e4bca2673e66c270bc
Developers@Work <gopinadh199922@gmail.com>
2017-06-08 10:57:23 +0530

    Create script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
  var subscriberCount = [],viewCount=[],commentCount=[],videoCount = [];
- 
-$(document).ready(function(){
-	this.keyword = 'Developers@Work';
-	searchCode(this.keyword);
-	var i=0;
-	//for(i=0;i<temp.length;i++){
-		countCode(channelId[i] , i);
-		displaychannelscount(i);
-	//}
-});
 
 function getNewChannel(){
 	document.getElementById('display').innerHTML = '';
