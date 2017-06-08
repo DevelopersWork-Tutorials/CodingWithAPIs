f1f4bf993405534aed6973da7a58ee675337dc78
Developers@Work <gopinadh199922@gmail.com>
2017-06-08 10:57:47 +0530

    Create docready.js

---
+++ docready.js
@@ -0,0 +1,10 @@
+ 
+$(document).ready(function(){
+	this.keyword = 'Developers@Work';
+	searchCode(this.keyword);
+	var i=0;
+	//for(i=0;i<temp.length;i++){
+		countCode(channelId[i] , i);
+		displaychannelscount(i);
+	//}
+});
